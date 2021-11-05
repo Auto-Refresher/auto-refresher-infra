b6f2550a3726487cee2e46d0689ea05ca9476f69
Harry morgan <harrywm12@gmail.com>
2021-11-05 17:05:28 +0000

    creds ???

--- provider.tf
+++ provider.tf
@@ -1,8 +1,12 @@
 terraform {
-  required_version = "~> 0.12.8"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.63.0"
+    }
+  }
 }
 
 provider "aws" {
-  version = "~> 3.63.0"
   region = "eu-west-2"
 }
